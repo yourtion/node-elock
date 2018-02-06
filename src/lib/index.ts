a32910bb48533ebe5bb3ec5214eac27eae6e2622
YourtionGuo <yourtion@gmail.com>
2018-02-06 23:29:43 +0800

    feat: add get method and add doc

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,6 +4,7 @@
  * @file ELock
  * @author Yourtion Guo <yourtion@gmail.com>
  */
+import Promise = require("bluebird");
 import { Redis } from "ioredis";
 
 export interface IELockOptions {
@@ -11,30 +12,83 @@ export interface IELockOptions {
   timeout?: number;
 }
 
+/**
+ * Easy use distributed redis lock implementation
+ *
+ * @class ELock
+ */
 class ELock {
 
   private client: Redis;
   private prefix: string;
   private timeout: number;
 
+  /**
+   * Creates an instance of ELock.
+   * @param {Redis} redis ioredis client
+   * @param {IELockOptions} options options
+   * @memberof ELock
+   */
   constructor(redis: Redis, options: IELockOptions) {
     this.client = redis;
     this.prefix = options.prefix ? "l:" + options.prefix : "l:" + new Date().getTime();
     this.timeout = options.timeout || 1000;
   }
 
+  /**
+   * Acquire a lock (fail will throw a error)
+   *
+   * @param {string} [key="default"] lock key
+   * @param {number} [timeout=this.timeout] lock time (ms)
+   * @returns {Promise}
+   * @memberof ELock
+   */
   public acquire(key = "default", timeout: number = this.timeout) {
     return this.client.set(this.getKey(key), new Date().getTime(), "PX", timeout, "NX");
   }
 
+  /**
+   * Get a lock (fail will return null)
+   *
+   * @param {string} [key="default"] lock key
+   * @param {number} [timeout=this.timeout] lock time (ms)
+   * @returns {Promise}
+   * @memberof ELock
+   */
+  public get(key = "default", timeout: number = this.timeout) {
+    return this.client.set(this.getKey(key), new Date().getTime(), "PX", timeout, "NX");
+  }
+
+  /**
+   * Release the lock
+   *
+   * @param {string} [key="default"] lock key
+   * @returns @returns {Promise}
+   * @memberof ELock
+   */
   public release(key = "default") {
     return this.client.del(this.getKey(key));
   }
 
+  /**
+   * Check is locked
+   *
+   * @param {string} [key="default"] lock key
+   * @returns {Promise}
+   * @memberof ELock
+   */
   public locked(key = "default") {
     return this.client.get(this.getKey(key)).then((res) => !!res);
   }
 
+  /**
+   * Get the real lock key in redis
+   *
+   * @private
+   * @param {string} key
+   * @returns {string}
+   * @memberof ELock
+   */
   private getKey(key: string) {
     return this.prefix + ":" + key;
   }
